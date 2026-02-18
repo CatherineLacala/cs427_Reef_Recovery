76e77c689db2d3ac2b4cc62b036f3808256f852e
M4CHEN <M4444Chen@gmail.com>
2026-02-17 21:07:04 -0600

    add scripts folder, add tool switching script
    now we can switch tool by pressing
    middle mouse button (only ui, the tool kit canvas switching now)

---
+++ Assets/Scripts/ToolSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class ToolSwitcher : MonoBehaviour
+{
+    [Header("ToolKit")]
+    public GameObject[] toolIcons;
+    public GameObject[] toolOutlines;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        currentIndex = toolIcons.Length;
+        UpdateToolUI();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            SwitchNext();
+        }
+    }
+
+    public void SwitchNext()
+    {
+        currentIndex = (currentIndex + 1) % (toolIcons.Length + 1);
+        UpdateToolUI();
+    }
+
+    void UpdateToolUI()
+    {
+        bool isBareHanded = (currentIndex == toolIcons.Length);
+        for (int i = 0; i < toolIcons.Length; i++)
+        {
+            bool shouldShow = !isBareHanded && (i == currentIndex);
+            toolOutlines[i]?.SetActive(shouldShow);
+        }
+        if (isBareHanded)
+        {
+            Debug.Log("NoTool");
+        }
+        else
+        {
+            Debug.Log("Switched to:" + toolIcons[currentIndex].name);
+        }
+    }
+}

d61ca9f1de8f072c58f339ae3c6813fb1e9e2a6d
Catherine Lacala <claca2@uic.edu>
2026-02-18 12:18:50 -0600

    skybox

---
+++ Assets/Scripts/SkyboxSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxSwitch : MonoBehaviour
+{
+    public Material sky1;
+    public Material sky2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y)) {
+            RenderSettings.skybox = sky1;
+            
+        }
+
+    }
+}

fbbfd4f18b38065c0677d79ee684ce80243adac6
Evelyn Johnson <evy.su.mei@gmail.com>
2026-02-24 15:28:27 -0600

    Added a clickable button and animated boat to travel to the center of the area.

---
+++ Assets/Scripts/BoatTransitManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatTransitManager : MonoBehaviour
+{
+    [Header("Player & Boat References")]
+    public GameObject playerController;
+    public GameObject boat;
+    public Transform playerBoatPosition;
+    public Transform boatDropoff;
+
+    [Header("Button References")]
+    public GameObject headToReefButton;
+    public GameObject setUpSolarPanelButton;
+
+    [Header("Settings")]
+    public float transitDuration = 5.0f;
+
+    private bool hasClicked = false;
+
+    public void StartBoatTransit()
+    {
+        if (hasClicked) return;
+        hasClicked = true;
+
+        StartCoroutine(TransitSequence());
+    }
+
+    private IEnumerator TransitSequence()
+    {
+        headToReefButton.SetActive(false);
+
+        TogglePlayerMovement(false);
+
+        playerController.transform.position = playerBoatPosition.position;
+        playerController.transform.rotation = playerBoatPosition.rotation;
+        playerController.transform.SetParent(boat.transform);
+
+        Vector3 startPos = boat.transform.position;
+        Quaternion startRot = boat.transform.rotation;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < transitDuration)
+        {
+            boat.transform.position = Vector3.Lerp(startPos, boatDropoff.position, elapsedTime / transitDuration);
+            boat.transform.rotation = Quaternion.Slerp(startRot, boatDropoff.rotation, elapsedTime / transitDuration);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        boat.transform.position = boatDropoff.position;
+        boat.transform.rotation = boatDropoff.rotation;
+
+        playerController.transform.SetParent(null);
+
+        TogglePlayerMovement(true);
+        setUpSolarPanelButton.SetActive(true);
+    }
+
+    private void TogglePlayerMovement(bool canMove)
+    {
+        CharacterController cc = playerController.GetComponent<CharacterController>();
+        if (cc != null)
+        {
+            cc.enabled = canMove;
+        }
+
+        Rigidbody rb = playerController.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = !canMove;
+        }
+    }
+}
---
+++ Assets/Scripts/CAVE2ButtonTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CAVE2ButtonTrigger : CAVE2Interactable
+{
+    public CAVE2.Button clickButton = CAVE2.Button.Button3;
+
+    public UnityEvent onClick;
+
+    // This detects the CAVE2 Wand click
+    // Debugging help
+    new void OnWandButtonDown(CAVE2.WandEvent evt)
+    {
+        if (evt.button == clickButton)
+        {
+            Debug.Log("Wand clicked the button!");
+            onClick.Invoke();
+        }
+    }
+}

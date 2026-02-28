84afae215a5b642f9b5a51a3f6dd2f8798be3871
Catherine Lacala <claca2@uic.edu>
2026-02-28 17:59:44 -0600

    diver narration for above water

---
+++ Assets/Scripts/BoatAudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSource;
+
+    public AudioClip boatIntroClip;
+    public AudioClip solarPanelClip;
+    public AudioClip reefClip;
+
+    private bool boatAudioPlayed = false;
+    
+    void Start()
+    {
+        StartCoroutine(PlayIntroAfterDelay());
+    }
+    IEnumerator PlayIntroAfterDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        audioSource.PlayOneShot(boatIntroClip);
+    }
+
+    public void PlaySolarPanelAudio()
+    {
+        if (!boatAudioPlayed)
+        {
+            audioSource.PlayOneShot(solarPanelClip);
+            boatAudioPlayed = true;
+        }
+    }
+
+    public void PlayReefAudio()
+    {
+        audioSource.PlayOneShot(reefClip);
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
---
+++ Assets/Scripts/BoatTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatTrigger : MonoBehaviour
+{
+    public BoatAudio audioManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioManager.PlaySolarPanelAudio();
+        }
+    }
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}

269fd7b2170f062e8b448e9834d4e29845c89a22
Catherine Lacala <claca2@uic.edu>
2026-03-11 12:18:39 -0500

    added blinking light to speaker sign

---
+++ Assets/Scripts/BlinkingLight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections; // Required for IEnumerator
+
+public class BlinkingLight : MonoBehaviour
+{
+    private Light myLight;
+    public float blinkInterval = 0.5f; // Time in seconds between blinks
+    private Coroutine blinkCoroutine;
+    void Start()
+    {
+        // Get the Light component attached to this GameObject
+        myLight = GetComponent<Light>();
+        // Start the blinking coroutine
+        blinkCoroutine = StartCoroutine(BlinkRoutine());
+    }
+
+    IEnumerator BlinkRoutine()
+    {
+        while (true) // Infinite loop to keep blinking
+        {
+            myLight.enabled = !myLight.enabled; // Toggle the light on/off
+            yield return new WaitForSeconds(blinkInterval); // Wait for the specified interval
+        }
+    }
+
+    // when button clicked, turn off light
+    public void TurnOffLight()
+    {
+        if (blinkCoroutine != null)
+        {
+            StopCoroutine(blinkCoroutine);
+        }
+
+        myLight.enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlaceSpeakerButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceSpeaker : MonoBehaviour
+{
+    public BlinkingLight blinkingLight;
+
+    public void PressButton()
+    {
+        if (blinkingLight != null)
+        {
+            blinkingLight.TurnOffLight();
+        }
+    }
+}

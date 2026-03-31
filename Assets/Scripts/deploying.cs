2cdaa6c5e832397ddaa3972fcc1b8a7663289287
000brian <brianmaciejwilk@gmail.com>
2026-03-30 22:39:32 -0500

    update speaker and spawning logic for speaker

--- Assets/Scripts/deploying.cs
+++ Assets/Scripts/deploying.cs
@@ -12,13 +12,26 @@ public class deploying : CAVE2Interactable
         {
             Transform wand = CAVE2.GetWandObject(evt.wandID).transform;
             deployableItem heldItem = wand.GetComponentInChildren<deployableItem>();
+
             if (heldItem != null && heldItem.IsGrabbed)
             {
                 heldItem.Consume();
+
                 if (prefabToDeploy != null)
                 {
-                    Instantiate(prefabToDeploy, transform.position, transform.rotation);
+                    Vector3 spawnPosition = transform.position;
+
+                    // shoot a raycast down
+                    if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit))
+                    {
+                        // when we hit the ground, make that where it spawns
+                        spawnPosition = hit.point;
+                    }
+
+
+                    Instantiate(prefabToDeploy, spawnPosition, transform.rotation);
                 }
+
                 Destroy(gameObject);
             }
         }

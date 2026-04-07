89ce291a12eedaa6ecd00daa17c0f1bbe9b76375
M4CHEN <M4444Chen@gmail.com>
2026-04-06 23:25:34 -0500

    speaker word stuffs

--- Assets/Scripts/deployableItem.cs
+++ Assets/Scripts/deployableItem.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class deployableItem : CAVE2Interactable
 {
+    public static deployableItem currentlyHeldItem = null;
+
     [Header("Grab Settings")]
     [SerializeField] CAVE2.Button grabButton = CAVE2.Button.Button3;
-    [SerializeField] float shrinkScale = 0.02f;
+    [SerializeField] float shrinkScale = 0.5f;
     [SerializeField] float shrinkDuration = 0.25f;
-    [SerializeField] Vector3 screenOffset = new Vector3(-0.2f, -0.05f, 0.1f);
+    [SerializeField] Vector3 screenOffset = new Vector3(0f, 0.5f, 0f);
 
     [Header("Rotation Settings")]
     [SerializeField] bool enableRotation = true;
@@ -33,22 +35,17 @@ public class deployableItem : CAVE2Interactable
 
         if (enableRotation)
         {
-            if (IsGrabbed)
-            {
-                transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime, Space.Self);
-            }
-            else
-            {
-                Vector3 currentWorldCenter = transform.TransformPoint(localCenterOffset);
-                Vector3 currentWorldAxis = transform.TransformDirection(rotationAxis);
-                transform.RotateAround(currentWorldCenter, currentWorldAxis, rotationSpeed * Time.deltaTime);
-            }
+            Vector3 currentWorldCenter = transform.TransformPoint(localCenterOffset);
+            Vector3 currentWorldAxis = transform.TransformDirection(rotationAxis);
+            transform.RotateAround(currentWorldCenter, currentWorldAxis, rotationSpeed * Time.deltaTime);
         }
+
     }
 
     void CalculateGroupCenter()
     {
         Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
         if (renderers.Length == 0)
         {
             localCenterOffset = Vector3.zero;
@@ -56,28 +53,31 @@ public class deployableItem : CAVE2Interactable
         }
 
         Bounds bounds = renderers[0].bounds;
+
         for (int i = 1; i < renderers.Length; i++)
         {
             bounds.Encapsulate(renderers[i].bounds);
         }
+
         localCenterOffset = transform.InverseTransformPoint(bounds.center);
     }
 
     new void OnWandButtonDown(CAVE2.WandEvent evt)
     {
-        if (evt.button == grabButton && !IsGrabbed)
+
+        if (evt.button == grabButton && !IsGrabbed && currentlyHeldItem == null)
         {
             Transform grabbingWand = CAVE2.GetWandObject(evt.wandID).transform;
             GrabObject(grabbingWand);
         }
+
     }
 
     void GrabObject(Transform wand)
     {
         IsGrabbed = true;
-
+        currentlyHeldItem = this;
         if (itemCollider != null) itemCollider.enabled = false;
-
         Rigidbody rb = GetComponent<Rigidbody>();
 
         if (rb != null)
@@ -97,17 +97,34 @@ public class deployableItem : CAVE2Interactable
     {
         Vector3 startScale = transform.localScale;
         float elapsedTime = 0;
+
         while (elapsedTime < shrinkDuration)
         {
             transform.localScale = Vector3.Lerp(startScale, targetScale, elapsedTime / shrinkDuration);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
+
         transform.localScale = targetScale;
     }
 
     public void Consume()
     {
+
+        if (currentlyHeldItem == this)
+        {
+            currentlyHeldItem = null;
+        }
+
         Destroy(gameObject);
+    }
+    private void OnDestroy()
+    {
+
+        if (currentlyHeldItem == this)
+        {
+            currentlyHeldItem = null;
+        }
+
     }
 }
